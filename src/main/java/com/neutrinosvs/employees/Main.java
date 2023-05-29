0227cacf9e62800bd647b5d21caacef898ad0e6b
vitsto <vitsto94@gmail.com>
2023-05-29 22:27:41 +1000

    linked list

--- src/main/java/com/neutrinosvs/employees/Main.java
+++ src/main/java/com/neutrinosvs/employees/Main.java
@@ -2,6 +2,9 @@ package com.neutrinosvs.employees;
 
 import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 
 public class Main {
@@ -9,20 +12,20 @@ public class Main {
         String peopleText = """
                 Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
                 Flinstone, Fred, 1/1/1900, Programmerzzzz, {locpd=2000,yoe=10,iq=140}
-                Flinstone2, Fred, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
-                Flinstone3, Fred, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
-                Flinstone4, Fred, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
-                Flinstone5, Fred, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
+                Flinstone2, Fred2, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
+                Flinstone3, Fred3, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
+                Flinstone4, Fred4, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
+                Flinstone5, Fred5, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
                 Rubble, Barney, 2/2/1905, Manager, {orgSize=300,dr=10}
-                Rubble2, Barney, 2/2/1905, Manager, {orgSize=100,dr=4}
-                Rubble3, Barney, 2/2/1905, Manager, {orgSize=200,dr=2}
-                Rubble4, Barney, 2/2/1905, Manager, {orgSize=500,dr=8}
-                Rubble5, Barney, 2/2/1905, Manager, {orgSize=175,dr=20}
+                Rubble2, Barney2, 2/2/1905, Manager, {orgSize=100,dr=4}
+                Rubble3, Barney3, 2/2/1905, Manager, {orgSize=200,dr=2}
+                Rubble4, Barney4, 2/2/1905, Manager, {orgSize=500,dr=8}
+                Rubble5, Barney5, 2/2/1905, Manager, {orgSize=175,dr=20}
                 Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=3}
-                Flinstone2, Wilma, 3/3/1910, Analyst, {projectCount=4}
-                Flinstone3, Wilma, 3/3/1910, Analyst, {projectCount=5}
-                Flinstone4, Wilma, 3/3/1910, Analyst, {projectCount=6}
-                Flinstone5, Wilma, 3/3/1910, Analyst, {projectCount=9}
+                Flinstone2, Wilma2, 3/3/1910, Analyst, {projectCount=4}
+                Flinstone3, Wilma3, 3/3/1910, Analyst, {projectCount=5}
+                Flinstone4, Wilma4, 3/3/1910, Analyst, {projectCount=6}
+                Flinstone5, Wilma5, 3/3/1910, Analyst, {projectCount=9}
                 Rubble, Betty, 4/4/1915, CEO, {avgStockPrice=300}
                 """;
 
@@ -30,22 +33,26 @@ public class Main {
 
         int totalSalaries = 0;
         IEmployee employee;
+        List<IEmployee> employees = new LinkedList<>();
         while (peopleMat.find()) {
             employee = Employee.createEmployee(peopleMat.group());
-//            if (employee instanceof Programmer prog) {
-//                System.out.println(prog.getIq());
-//            } else if (employee instanceof Manager manager) {
-//                System.out.println();
-//            } else if (employee instanceof Analyst) {
-//                System.out.println();
-//            } else if (employee instanceof CEO) {
-//                System.out.println();
-//            } else {
-//                System.out.println("Default output");
-//            }
-            System.out.println(employee);
-            totalSalaries += employee.getSalary();
+            employees.add(employee);
         }
+
+        List<String> removalNames = new ArrayList<>();
+        removalNames.add("Wilma5");
+        removalNames.add("Barney4");
+        removalNames.add("Fred2");
+
+       for (IEmployee worker : employees) {
+           if (worker instanceof Employee tmpWorker) {
+               if (removalNames.contains(tmpWorker.firstName)) {
+                   employees.remove(worker);
+               }
+           }
+           System.out.println(worker);
+           totalSalaries += worker.getSalary();
+       }
 
         NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
         System.out.printf("The total payout should be %s%n", currencyInstance.format(totalSalaries));
